2b03ba4617b0e72187c5f2ef1fadc9ffdb79fbc9
Adel LATIBI <alatibi@campus-eni.fr>
2022-06-01 11:50:41 +0200

    demos

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
     namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,34 @@ use Symfony\Component\Routing\Annotation\Route;
         /**
          * @Route("/", name="app_home")
          */
-        public function home():Response{
+        public function home(Request $request):Response{
+            
+            //dd($request->request->get("age"));// post
+            //dd($request->query->get("age"));// get
             return $this->render("main/home.html.twig");
         }
 
+        /**
+         * @Route("/traitement", name="app_traitement",methods={"POST"})
+         */
+        public function traitement(Request $request):Response{
+            
+            dd($request->request->get("prenom"));// post
+           
+        }
+
+
+
+        /**
+         * @Route("/contact/{nom}", 
+         * name="app_contact",
+         * requirements={"nom"="[a-zA-Z0-9]+"},
+         * methods={"GET","POST"})
+         */
+        public function contact($nom):Response{
+            // BDD requette select * from users where prenom = $nom
+            return $this->json($nom);
+        }        
 
     }
 
