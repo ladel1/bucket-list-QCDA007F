7f0f8a25633ee5dacc09b03bc5973cab5260cbdc
Adel LATIBI <alatibi@campus-eni.fr>
2022-06-01 10:08:35 +0200

    Correction Tp 2

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php 
+    namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+    class MainController extends AbstractController{
+
+
+        /**
+         * @Route("/", name="app_home")
+         */
+        public function home():Response{
+            return $this->render("main/home.html.twig");
+        }
+
+
+    }
+
+
